239cb78c6294ed0b599cb9edb48daab91c54e72c
Lucca Livino <lucca.f.livino@gmail.com>
2025-06-05 18:09:41 -0400

    Adicionando dados

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,20 @@ import './style.css'
 
 const params = new URLSearchParams(window.location.search)
 const id = params.get("id")
+const result = await fetch(`http://localhost:5173/dados.json`)
+const body = await result.json()
 
-const app = document.querySelector<HTMLDivElement>('#app')!
 
-app.innerHTML = `Olá ${id}`
+const usuario = body.usuarios.find((u: any) => u.id === id)
+
+if (usuario) {
+  const imgContainer = document.querySelector<HTMLDivElement>("#Cimg")
+  if (imgContainer) {
+    imgContainer.innerHTML = `
+    <div>
+        <img src="${usuario.foto_fundo}">
+    </div>
+`}
+  }else{
+    console.error("Usuário não encontrado")
+  }
