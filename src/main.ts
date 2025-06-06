ddea0aa262ec0acead31f690ffe433374790ca94
Lucca <lucca.f.livino@gmail.com>
2025-06-05 21:02:14 -0400

    adicionando background e foto de perfil

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,34 @@
 import './style.css'
 
 
-const params = new URLSearchParams(window.location.search)
-const id = params.get("id")
-const result = await fetch(`http://localhost:5173/dados.json`)
+const url = new URL(window.location.href);
+const params = url.searchParams
+// const params = new URLSearchParams(window.location.search)
+const id = params.get("id") as string;
+const result = await fetch(`http://localhost:3000/usuarios/`)
 const body = await result.json()
+// const idPo = id[0]
 
+// const usuario = body.usuarios.find((u: any) => u.id === id)
 
-const usuario = body.usuarios.find((u: any) => u.id === id)
-
-if (usuario) {
+// if (usuario) {
+//   console.log(usuario.nome)
   const imgContainer = document.querySelector<HTMLDivElement>("#Cimg")
+  const botaoContainer = document.querySelector<HTMLDivElement>("#Cbotao")
   if (imgContainer) {
-    imgContainer.innerHTML = `
-    <div>
-        <img src="${usuario.foto_fundo}">
+    imgContainer.innerHTML = 
+    `   <div class="container-imagem"style="background-image: ${body[0].foto_fundo};">
+        <img src="${body[0].foto_url}" alt="Foto de perfil" class="perfil">
     </div>
-`}
-  }else{
-    console.error("Usuário não encontrado")
-  }
+    
+
+
+    
+
+    
+    `
+  } 
+//   }else{
+//     console.error("Usuário não encontrado")
+//   }
+// }
