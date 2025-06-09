40a62b39a5a87b2adf914c7465c29ff6f6652d5e
Lucca Livino <lucca.f.livino@gmail.com>
2025-06-09 17:33:50 -0400

    Finalizando toda a estilização e outros usuários

--- src/main.ts
+++ src/main.ts
@@ -1,53 +1,56 @@
 import './style.css'
 
-
 const url = new URL(window.location.href);
-const params = url.searchParams
+const params = url.searchParams;
 const id = params.get("id") as string;
-const result = await fetch(`http://localhost:3000/usuarios/`)
-const body = await result.json()
-const idIndex = parseInt(id) - 1
+const result = await fetch(`http://localhost:3000/usuarios/`);
+const body = await result.json();
+const idIndex = parseInt(id) - 1;
 
+const imgContainer = document.querySelector<HTMLDivElement>("#Cimg");
 
-const imgContainer = document.querySelector<HTMLDivElement>("#Cimg")
+if (imgContainer && id !== null && body[idIndex]) {
 
+  document.body.style.background = body[idIndex].cor_fundo;
 
-if (imgContainer && id !== null) {
+  const contornoEstilo = body[idIndex].contorno ? `border: 2px solid ${body[idIndex].contorno};` : '';
+
+  const hoverBotao = body[idIndex].hover_botao;
+  
+  const botoesHTML = (body[idIndex].links as any[]).map((link) => `
+    <a href="${link.url}">
+      <div class="botao" style="background: ${body[idIndex].cor_botao}; border-radius: ${body[idIndex].border}; ${contornoEstilo}; ${hoverBotao} ">
+        <img src="${link.icone}" class="icone" ">
+        <p class="texto" style="color: ${body[idIndex].cor_texto}">${link.tipo}</p>
+      </div>
+    </a>
+  `).join("");
+
   imgContainer.innerHTML = `
+
     <div class="container-imagem" style="background-image: ${body[idIndex].foto_fundo};">
-      <img src="${body[idIndex].foto_url}" alt="Foto de perfil" class="perfil" width="100px" height="100px";>
+      <img src="${body[idIndex].foto_url}" alt="Foto de perfil" class="perfil" width="100px" height="100px">
       <h1>${body[idIndex].nome}</h1>
       <div class="container-botao">
-        <a href="${body[idIndex].links[0].url}">
-          <div class="botao" style="background-color: ${body[idIndex].cor_botao}; border-radius: ${body[idIndex].border};">
-          <img src="/Blog.png" class= "icone">
-            <p class="texto">Blog</p>
-          </div>
-        </a>
-        <a href="${body[idIndex].links[1].url}">
-          <div class="botao" style="background-color: ${body[idIndex].cor_botao}; border-radius: ${body[idIndex].border};">
-            <img src="/Blog.png" class= "icone">
-            <p>GitLab</p>
-          </div>
-        <a href="${body[idIndex].links[2].url}">
-          <div class="botao" style="background-color: ${body[idIndex].cor_botao}; border-radius: ${body[idIndex].border};">
-            <p>Github</p>
-          </div>
-        <a href="${body[idIndex].links[3].url}">
-          <div class="botao" style="background-color: ${body[idIndex].cor_botao}; border-radius: ${body[idIndex].border};">
-            <p>Linkedin</p>
-          </div>
-        <a href="${body[idIndex].links[4].url}">
-          <div class="botao" style="background-color: ${body[idIndex].cor_botao}; border-radius: ${body[idIndex].border};">
-            <p>Instagram</p>
-          </div>
-        <a href="${body[idIndex].links[5].url}">
-          <div class="botao" style="background-color: ${body[idIndex].cor_botao}; border-radius: ${body[idIndex].border};">
-            <p>Enviar um e-mail</p>
-          </div>
-      </a>
+        ${botoesHTML}
+      </div>
+      <div class="container-qrcode">
+        <img src="${body[idIndex].qr_code}" class="qrcode">
       </div>
     </div>
-  `
-}
+  `;
 
+  const botoes = imgContainer.querySelectorAll<HTMLDivElement>('.botao');
+
+  botoes.forEach(botao => {
+    const bgOriginal = botao.style.background;
+
+    botao.addEventListener('mouseenter', () => {
+      botao.style.background = hoverBotao;
+    });
+
+    botao.addEventListener('mouseleave', () => {
+      botao.style.background = bgOriginal;
+    });
+  });
+}
